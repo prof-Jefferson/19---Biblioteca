e66251f20c8a596a4b343324e24aab9edad91ba7
prof-Jefferson <121240091+prof-Jefferson@users.noreply.github.com>
2023-04-12 08:30:10 -0300

    Cadastro
    Implementando função específica cadastro.

--- Program.cs
+++ Program.cs
@@ -49,8 +49,63 @@ namespace Biblioteca
 						Console.ReadKey();
 						break;
 				}
+			}			
+		}
+		
+		static void Cadastro(Biblioteca biblioteca)
+		{
+			Console.Clear();
+			Console.WriteLine("Selecione uma opção de cadastro:");
+			Console.WriteLine("1 - Cadastrar cliente");
+			Console.WriteLine("2 - Cadastrar livro");
+			Console.WriteLine("0 - Voltar");
+
+			int.TryParse(Console.ReadLine(), out int opcao);
+
+			switch (opcao)
+			{
+				case 1:
+					Console.Clear();
+					Console.WriteLine("Cadastro de Cliente:");
+					Console.Write("Nome: ");
+					string nomeCliente = Console.ReadLine();
+					Console.Write("Data de nascimento (AAAA-MM-DD): ");
+					DateTime.TryParse(Console.ReadLine(), out DateTime dataNascimento);
+					Console.Write("Telefone: ");
+					string telefone = Console.ReadLine();
+
+					Cliente novoCliente = new Cliente { Id = biblioteca.clientes.Count + 1, Nome = nomeCliente, DataNascimento = dataNascimento, Telefone = telefone };
+					biblioteca.clientes.Add(novoCliente);
+
+					Console.WriteLine("Cliente cadastrado com sucesso!");
+					Console.ReadKey();
+					break;
+
+				case 2:
+					Console.Clear();
+					Console.WriteLine("Cadastro de Livro:");
+					Console.Write("Título: ");
+					string titulo = Console.ReadLine();
+					Console.Write("Autor: ");
+					string autor = Console.ReadLine();
+
+					Livro novoLivro = new Livro { Id = biblioteca.livros.Count + 1, Titulo = titulo, Autor = autor, Disponivel = true };
+					biblioteca.livros.Add(novoLivro);
+
+					Console.WriteLine("Livro cadastrado com sucesso!");
+					Console.ReadKey();
+					break;
+
+				case 0:
+					// Retorna ao menu principal
+					break;
+
+				default:
+					Console.WriteLine("Opção inválida! Pressione qualquer tecla para continuar...");
+					Console.ReadKey();
+					break;
 			}
-		}		
+		}
 		
 	}
 }
